65c06f7aa2fb33661d95a33abc236f9838b28e3f
Ryan Corson <ryan.corson88@gmail.com>
2025-08-13 01:47:41 -0500

    adds motivation

--- Python Scripts/motivationalProgram.py
+++ Python Scripts/motivationalProgram.py
@@ -1,2 +1,7 @@
-print('If you wish to code from scratch...')
-print('You must first invent the universe.')
+wish = 'code from scratch'
+invention = 'invent the universe.'
+
+def create_universe(pie, starStuff):
+    return f'If you wish to {pie}...\nYou must first {starStuff}'
+
+print(create_universe(wish, invention))
